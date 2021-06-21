90f7cf23f6087ad7b03ad47a84321fc1a5a94cdd
André <andrevictor50@gmail.com>
2021-06-21 09:39:12 -0300

    feat: adding basic files for app

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const Content = styled.View``;
+
+export const Heading = styled.Text``;
+
+export const Paragraph = styled.Text``;
+
+export const Image = styled.Image``;
